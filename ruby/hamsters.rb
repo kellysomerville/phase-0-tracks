a7d9fb44dfa7056ffe5f5579f4dd7fd7b75ba56a
Kelly Somerville <kellynsomerville@gmail.com>
2017-02-15 17:06:36 -0800

    Adds integer string conversion

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,3 +4,19 @@ fur_color = ""
 adoptable = ""
 hamster_age = nil
 
+puts "Whats the hamster name?"
+h_name = gets.chomp
+
+puts "How loud is the hamster squeek ? 1-10"
+volume_level = gets.chomp
+puts volume_level.to_i
+
+puts "What color os the fur?"
+fur_color = gets.chomp
+
+puts "Is the hamster adoptable ? y/n"
+adoptable = gets.chomp
+
+puts "How old is the hamster ?"
+hamster_age gets.chomp
+puts hamster_age.to_i
