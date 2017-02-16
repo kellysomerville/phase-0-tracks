240b24b3021abc6180999922b8833d2cf7d5a350
Kelly Somerville <kellynsomerville@gmail.com>
2017-02-15 17:38:13 -0800

    Fixes hamster_age error

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,16 +10,16 @@ puts " The hamsters name is #{h_name}."
 
 puts "How loud is the hamster squeek ? 1-10"
 volume_level = gets.chomp
-puts "The hamster squeek is at a volume of #{volume_level.to_i}"
+puts "The hamster squeek is at a volume of #{volume_level.to_i}."
 
 puts "What color os the fur?"
 fur_color = gets.chomp
-puts "The color of the hamster is #{fur_color}"
+puts "The color of the hamster is #{fur_color}."
 
 puts "Is the hamster adoptable ? yes/no"
 adoptable = gets.chomp
 puts "#{adoptable} adoptable"
 
 puts "How old is the hamster ?"
-hamster_age gets.chomp
+hamster_age = gets.chomp
 puts "The hamster age is #{hamster_age.to_i}."
