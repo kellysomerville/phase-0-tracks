62c2a423cf7627a2a98f770c398043de504ad7ae
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-01 10:37:09 -0800

    Adds puppy class- attributes and methods

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+#SPECIES -------------------------
+#PUPPY
+
+#CHARACTERISTICS------------------
+#Fur/hair length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#color: varies
+#legs: 4
+#ears: 2
+
+#Bahavior---------------------------
+#Jump: varies
+#snuggle: varies
+#run-alot: varies
+#barks: varies
+#plays in water: varies
+#fetch: varies
+#tricks: varies
