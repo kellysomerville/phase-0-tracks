8d09fdce4874572b49d3f7ce5de46135c3862194
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-11 13:13:30 -0800

    Adds Genie class and initialize w/ name and age

---
+++ kelly_n_somerville/genie.rb
@@ -0,0 +1,15 @@
+
+# Genie class
+# initialize name and age
+# write driver code 
+class Genie
+
+	def initialize(name,age)
+		@name = name
+		@age = age
+	end
+end
+
+bob = Genie.new("Bob", 888)
+
+youssef = Genie.new("Youssef", 999)
