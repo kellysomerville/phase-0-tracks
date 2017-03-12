74cbf07f25b6360e8ce7a004a051291843e9dafb
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-11 19:10:26 -0800

    Adds pseudocode for reverse a string function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+
+
+// assign my string variables:
+
+
+
+// design a reverse the string function
+// reverse a string :
+// split the string into an array
+// reverse the order of the array
+// join the array back together into the reverse of original string
+
+
+
+
+
+// call the function with assigned variables
