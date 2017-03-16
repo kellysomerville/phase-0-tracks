3a4faf363c4efb90240b77384978044615a38458
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-16 12:59:19 -0700

    Adds first pseudocode and empty hash with keys

---
+++ ruby/hashes_Fiddler_Crabs.rb
@@ -0,0 +1,14 @@
+# write a program that will allow an interior designer to enter the details of a given client:
+
+#the client's name, age, number of children, decor theme
+#Prompt the designer/user for all of this information.
+# Convert any user input to the appropriate data type .to_s   for age and number of children
+# Print the hash back out to the screen when the designer has answered all of the questions
+
+interior_designer = {
+
+ 	name: " " , 
+ 	age:  ""  , 
+ 	number_of_children:  "" , 
+ 	decor_theme:  "" ,
+ }
