9892c71102c29dc20aa6251bda0fe9bbec2e25d2
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-17 13:30:59 -0700

    Adds pheudocode to fiddler crab nested data file

---
+++ ruby/nested_data_structures_fiddler_Crabs.rb
@@ -0,0 +1,11 @@
+# 5.4 nested data structure
+#design and build a nested data structure to represent a real-world construct.
+# use an assortment on arrays and hashes
+
+# dog training
+# three levels
+# class avalability integer
+# skills the dogs work on in the agility classes
+# add to the list
+# print access to inner levels
+# print entire hash
