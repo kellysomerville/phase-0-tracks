47651d5c2ec5a88e48ecf75190bb8fa02077f2c6
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-20 10:36:42 -0700

    Adds pseudocode Puppy Class behavio and characteristics

--- ruby/puppy_Fiddle_crabs.rb
+++ ruby/puppy_Fiddle_crabs.rb
@@ -1,3 +1,42 @@
+# # SPECIES ------------------------
+# # class Puppy
+
+# # CHARACTERISTICS ----------------
+# Eye count: 2
+# Friendly: varies
+# Fur/hair: varies
+# Name: varies
+# Color: varies
+# Legs: 4
+# Ears: 2
+# Tail_length: varies
+# Height: varies
+# Length: varies
+# Teeth: sharp
+# Sheds: varies
+
+# # BEHAVIOR -----------------------
+# Jumper: varies
+# Runner: varies
+# Fetch: varies
+# Energetic: varies
+# Likes_Water: varies
+# Pulls_on_leash: varies
+# Listens: varies
+# Barks: varies
+# Climbs: varies 
+# Walks_on_2_legs: varies
+
+
+
+
+
+
+
+#################################################
+#below is My bumble bee homework
+#################################################
+
 #SPECIES -------------------------
 #PUPPY
 
