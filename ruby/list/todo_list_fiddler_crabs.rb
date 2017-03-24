b13418b94ee9642583165bae5272a6a434693619
Kelly Somerville <kellynsomerville@gmail.com>
2017-03-24 15:17:06 -0700

    updated code and changed require rela to new fiddler file

--- ruby/list/todo_list_fiddler_crabs.rb
+++ ruby/list/todo_list_fiddler_crabs.rb
@@ -1,22 +1,45 @@
 class TodoList
-    #initialize method for our list
-    def initialize(list)
+    def initialize(list) 
         @list = list
     end
-    #create a method to store default items to our list
     def get_items
-        @list
+      @list
     end
-    #create a method to add an item to our list
-    def add_item(item)
-        @list << item 
+    def add_item(x)
+      @list << x
     end
-    #create a method to delete an item from our list
-    def delete_item(item)
-        @list.delete(item)
+    def delete_item(x)
+        @list.delete(x)
     end
-    #create a method to retieve an item via index
-    def get_item(index)
-        @list[index]
+    def get_item(i)
+        @list[i]
     end
 end
+
+
+
+##########################
+#below is my bumblebees
+
+# class TodoList
+#     #initialize method for our list
+#     def initialize(list)
+#         @list = list
+#     end
+#     #create a method to store default items to our list
+#     def get_items
+#         @list
+#     end
+#     #create a method to add an item to our list
+#     def add_item(item)
+#         @list << item 
+#     end
+#     #create a method to delete an item from our list
+#     def delete_item(item)
+#         @list.delete(item)
+#     end
+#     #create a method to retieve an item via index
+#     def get_item(index)
+#         @list[index]
+#     end
+# end
